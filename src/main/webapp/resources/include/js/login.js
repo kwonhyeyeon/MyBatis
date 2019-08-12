a9ff35928c6a3f62ddff7408adee0bd5327c30c5
InSub Lee <dldlstjq1212@gmail.com>
2019-08-12 09:57:52 +0900

    js 충돌 수정

--- src/main/webapp/resources/include/js/login.js
+++ src/main/webapp/resources/include/js/login.js
@@ -1,30 +1,4 @@
 $(function(){
-<<<<<<< HEAD
-	//아래 함수는 서버로 받아 온 코드값에 대한 처리 함수 (login.jsp 파일에 정의.)
-	errCodeCheck()
-	$('#userId, #userPw').bind("keyup", function(){
-			$(this).parents("div").find(".error").html("");
-	});
-	/*로그인 버튼 클릭시 처리 이벤트*/
-	$("#loginBtn").click(function() {
-		if (!formCheck($('#userId'), $('.error:eq(0)'), "아이디를")) return;
-		else if(!inputVerify(0, '#userId', '.error:eq(0)')) return;
-		else if(!formCheck($('#userPw'),$('.error:eq(1)'), "비밀번호를"))return;
-		else if(!inputVerify(1, "#userPw", '.error:eq(1)')) return;
-		else{
-			$("#loginForm").attr({
-				"method":"POST",
-				"action":"/member/login.do"
-			});
-			$("#loginForm").submit();
-		}
-	});
-	/*회원가입 버튼 클릭 시 처리 이벤트*/
-	$("#joinBtn").click(function(){
-		location.href="/member/join.do";
-	});
-});	
-=======
    //아래 함수는 서버로 받아 온 코드값에 대한 처리 함수 (login.jsp 파일에 정의.)
    errCodeCheck()
    $('#userId, #userPw').bind("keyup", function(){
@@ -64,7 +38,6 @@ function inputVerify(index, data, printarea) {
 		return true;
 	}
 }
->>>>>>> cd9e3bfe29da4a3a02e9e2b4997f882025dbbad8
 
 //유효성체크, 출력영역, 메시지내용
 function formCheck(main, item, msg){
@@ -76,8 +49,4 @@ function formCheck(main, item, msg){
       return true;
    }
 }
-<<<<<<< HEAD
-	
-=======
    
->>>>>>> cd9e3bfe29da4a3a02e9e2b4997f882025dbbad8
